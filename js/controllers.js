7380aa067cef172aba6111fb0112e233df0d468d
singhcharanjot <junkproof94@gmail.com>
2017-03-08 17:47:48 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -3,15 +3,26 @@ angular.module('starter.controllers', [])
 .controller('homeCtrl', function($scope,$rootScope,$http) {
 
  $scope.urlReference=firebase.database().ref('/myDatabse/pathDetails');
-
+$scope.loader=true;
   $http.get("https://motivationapp-51320.firebaseio.com/myDatabse.json")
     .then(function(response) {
+      $scope.loader=false;
+      if(response.data.pathDetails)
+      {
         $scope.myWelcome = response;
         // console.log(JSON.stringify($scope.myWelcome));
       $scope.imagePath=($scope.myWelcome.data.pathDetails).reverse();
+    }
+    else {
+      console.log('make a generic no datafound function');
+    }
+
         // alert($scope.myWelcome.data.pathDetails.length)
 
-    });
+    },function errorHandler(response)
+  {
+    console.log('make a generic no error function');
+  });
 
   // firebase.database().ref('/myDatabse/pathDetails').once('value').then(function(snapshot) {
   //
@@ -23,7 +34,7 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ChatsCtrl', function($scope, $http,$ionicModal,$rootScope,$cordovaFileTransfer) {
+.controller('ChatsCtrl', function($scope, $http,$ionicModal,$rootScope) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -31,16 +42,29 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+$scope.loader=true;
 
     $http.get("https://motivationapp-51320.firebaseio.com/myDatabse.json")
     .then(function(response) {
+      if(response.data.pathDetails)
+      {
+        $scope.loader=false;
         $scope.myWelcome = response;
         // console.log(JSON.stringify($scope.myWelcome));
       $scope.imagePath=($scope.myWelcome.data.pathDetails).reverse();
-      console.log($scope.imagePath);
+      console.log(response.data.pathDetails);
         // alert($scope.myWelcome.data.pathDetails.length)
+      }
 
-    });
+      else {
+        console.log('make a generic no datafound function');
+      }
+
+
+    },function error(response)
+  {
+  console.log(response+'make a generic no error function');
+  });
 
 
         $ionicModal.fromTemplateUrl('templates/modal.html', {
